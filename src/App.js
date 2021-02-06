93ea39deb7a1c91506643a9721b320669b2ca71a
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-02-07 00:44:10 +0330

    conditional rendering: add login component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Clock from './Clock';
 import ToggleButton from "./ToggleButton";
+import Login from './Login'
 
 function App() {
   return (
     <div className="App">
       <Clock />
       <ToggleButton />
+      <Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isLoggedIn : false};
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogin() {
+        this.setState({isLoggedIn : true});
+    }
+
+    handleLogout() {
+        this.setState({isLoggedIn : false});
+    }
+
+    render() {
+        // inline if-else statement
+        const button = this.state.isLoggedIn ? 
+         <button onClick={this.handleLogout} >Logout</button> :
+         <button onClick={this.handleLogin} >Login</button>;
+
+         return (
+             <>
+                {button}
+                {/* inline if statement */}
+                {this.state.isLoggedIn && 
+                <div>
+                    welcome to your dashboard
+                </div>
+                }
+             </>
+         )
+    }
+}
