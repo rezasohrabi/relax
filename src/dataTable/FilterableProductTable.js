b8b6d179583a557e396c8ed28890bd9962ca1493
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-02-08 21:37:55 +0330

    thinking in react: update state and add interactivity to project

--- src/dataTable/FilterableProductTable.js
+++ src/dataTable/FilterableProductTable.js
@@ -9,17 +9,30 @@ export default class FilterableProductTable extends React.Component {
             filterText: '',
             inStockOnly: false
         }
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+        this.handleInStockChange = this.handleInStockChange.bind(this);
     }
+
+    handleFilterTextChange(filterText) {
+        this.setState({filterText : filterText});
+    }
+
+    handleInStockChange(inStockOnly) {
+        this.setState({inStockOnly : inStockOnly});
+    }
+    
     render() {
         return (
             <section style={{margin: '20px', backgroundColor: '#efefef'}}>
             <SearchInput 
             filterText={this.state.filterText}
-            inStockOnly={this.state.inStockOnly}/>
+            inStockOnly={this.state.inStockOnly}
+            onFilterTextChange={this.handleFilterTextChange} />
             <ProductTable 
             products={this.props.products}
             filterText={this.state.filterText}
-            inStockOnly={this.state.inStockOnly} />
+            inStockOnly={this.state.inStockOnly} 
+            onInStockChange={this.handleInStockChange} />
             </section>
         )
     }
--- src/dataTable/SearchBar.js
+++ src/dataTable/SearchBar.js
@@ -1,16 +1,32 @@
 import React from 'react'
 
 export default class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+        this.handleInStockChange = this.handleInStockChange.bind(this);
+    }
+
+    handleFilterTextChange(e) {
+        this.props.onFilterTextChange(e.target.value);
+    }
+
+    handleInStockChange(e) {
+        this.props.onInStockOChange(e.target.value);
+    }
+
     render() {
         return (
             <>
             <input 
             type='text' 
             placeholder='search...' 
-            value={this.props.filterText} />
+            value={this.props.filterText} 
+            onChange={this.handleFilterTextChange} />
             <p>
                 <input type='checkbox' 
-                value={this.props.inStockOnly} />
+                value={this.props.inStockOnly} 
+                onChange={this.handleInStockChange}/>
                 {' '}
                 only show products in stock
             </p>
